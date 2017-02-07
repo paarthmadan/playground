58008620048ba81b522abd51c6c46963670f96aa
paarthmadan <madanhockey@hotmail.com>
2017-02-07 14:54:30 -0500

    J1 Finished

--- ccc2014/J1.java
+++ ccc2014/J1.java
@@ -1,5 +1,23 @@
 package ccc2014;
+import java.util.Scanner;
 
 public class J1 {
-
+	public static void main(String [] args){
+		Scanner input = new Scanner(System.in);
+		
+		int firstAngle = input.nextInt();
+		int secondAngle = input.nextInt();
+		int thirdAngle = input.nextInt();
+		
+		if((firstAngle + secondAngle + thirdAngle) != 180){
+			System.out.println("Error");
+		}else if(firstAngle == secondAngle && secondAngle == thirdAngle){
+			System.out.println("Equilateral");
+		}else if(firstAngle == secondAngle || secondAngle == thirdAngle || firstAngle == thirdAngle){
+			System.out.println("Isosceles");
+		}else{
+			System.out.println("Scalene");
+		}
+		
+	}
 }
