00990ac095ec5953dac738f91850e34f08197b46
paarthmadan <madanhockey@hotmail.com>
2017-02-10 17:11:44 -0500

    CCC Senior 2014 Started
    S1 Completed

---
+++ ccc2014/S1.java
@@ -0,0 +1,38 @@
+package ccc2014;
+import java.util.Scanner;
+
+public class S1 {
+	public static void main(String [] args){
+		Scanner input = new Scanner(System.in);
+		
+		int numberOfGuests = input.nextInt();
+		int roundsOfRemoval = input.nextInt();
+		
+		int[] guests = new int[numberOfGuests];
+		
+		for(int i = 0; i < guests.length; i++){
+			guests[i] = i + 1;
+		}
+		
+		for(int i = 0; i < roundsOfRemoval; i++){
+			int removalFactor = input.nextInt();
+			int counter = 0; 
+			for(int j = 0; j < guests.length; j++){
+				if(guests[j] != -1){
+					counter++;
+				}
+				
+				if(counter % removalFactor == 0){
+					guests[j] = -1;
+				}
+			}
+		}
+		
+		for(int i = 0; i < guests.length; i++){
+			if(guests[i] != -1){
+				System.out.println(guests[i]);
+			}
+		}
+		
+	}
+}
