e08fc44df9f333988e2bdc87fc284a472c26c13b
paarthmadan <madanhockey@hotmail.com>
2017-03-12 15:40:50 -0400

    Starting node, destination node created

--- dijkstra/Dijkstra.java
+++ dijkstra/Dijkstra.java
@@ -3,8 +3,14 @@ import java.util.*;
 
 public class Dijkstra {
 	
+	Vertex startingNode;
+	Vertex destinationNode;
+	
 	//ALGORITHM
 	public Dijkstra(List<Vertex> nodes, List<Edge> edges){
+		
+		this.startingNode = nodes.get(0);
+		this.destinationNode = nodes.get(nodes.size() - 1);
 		
 		//PRINTS ALL NODES
 		for(Vertex v: nodes)
@@ -12,6 +18,8 @@ public class Dijkstra {
 		
 		for(Edge e : edges)
 			System.out.println(e);
+		
+		System.out.println("Goal: " + startingNode.getName() + " --> " + destinationNode.getName());
 	}
 	
 }
