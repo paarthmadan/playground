2927666debc12c1354cfcd9809a120adcfdebcbf
paarthmadan <madanhockey@hotmail.com>
2017-07-24 16:19:49 -0400

    (J5):used memoization, and cached results to avoid redudancy

--- ccc2015/J5.java
+++ ccc2015/J5.java
@@ -2,27 +2,27 @@ package ccc2015;
 import java.util.Scanner;
 
 public class J5 {
+	private static int [][][] saved;
+	
 	public static void main(String [] args){
 		Scanner input = new Scanner(System.in);
 		int n = input.nextInt();
 		int k = input.nextInt();
 		
+		saved = new int[n + 1][k + 1][n];
+		
 		System.out.println(distribute(n, k, 1));
 	}
 	
 	public static int distribute(int n, int k, int min){
-		int ways = 0;
-		
-		if(n == k || k == 1){
-			ways = 1;
-		}else{
-//			System.out.println(n/k);
-			for(int i = min; i <= (n / k); i++){
-//				System.out.printf("d(%d, %d, %d)\n", (n-i), (k-1), i);
-				ways += distribute(n - i, k - 1, i);				
-			}
+		if(saved[n][k][min] == 0){
+			if(n == k || k == 1)
+				saved[n][k][min] = 1;
+			else
+				for(int i = min; i <= (n / k); i++)
+					saved[n][k][min] += distribute(n - i, k - 1, i);				
 		}
-		return ways;
+		return saved[n][k][min];
 	}
 	
 }
