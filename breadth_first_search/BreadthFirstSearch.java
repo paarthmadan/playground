ee09fef078fd00d93cfd313656ae7d568a35f97a
paarthmadan <madanhockey@hotmail.com>
2017-12-08 19:34:42 -0500

    (wip): bfs logic done
    debugging, test case 0 works, others don't

--- breadth_first_search/BreadthFirstSearch.java
+++ breadth_first_search/BreadthFirstSearch.java
@@ -1,7 +1,12 @@
 package breadth_first_search;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class BreadthFirstSearch {
+	
+	public final static int EDGE_WEIGHT = 6;
+	
 	public static void main(String [] args){
 		Scanner input = new Scanner(System.in);
 		int q = input.nextInt();
@@ -14,20 +19,50 @@ public class BreadthFirstSearch {
 			
 			for(int i = 0; i < n; i++)
 				nodes[i] = new Node(i + 1);
-			
+			int count = 0;
 			for(int edge = 0; edge < m; edge++){
 				int src = input.nextInt();
 				int dest = input.nextInt();
-				
+				if(src == 16){
+					
+					count++;
+				}
 				nodes[src - 1].addAdjacentNode(nodes[dest - 1]);
 			}
+			System.out.println(count);
 			int startingNode = input.nextInt();
+	
+			int [] distances = search(nodes, startingNode);
 			
-			for(int i = 0; i < nodes.length; i++){
-				for(int j = 0; j < nodes[i].getAdjacentNodes().size(); j++){
-					System.out.println(nodes[i].getLabel() + " --> " + nodes[i].getAdjacentNodes().get(j).getLabel());
+//			for(int d : distances)
+//				if(d != 0)
+//					System.out.print(d + " ");
+//			System.out.println();
+		}
+	}
+	public static int[] search(Node[] nodes, int startingNode){
+		
+		LinkedList<Node> queue = new LinkedList<>();
+		queue.add(nodes[startingNode - 1]);
+		
+		int [] distances = new int[nodes.length];
+		for(int d = 0; d < distances.length; d++)
+			distances[d] = -1;
+		
+		distances[startingNode - 1] = 0;
+		
+		while(!queue.isEmpty()){
+			Node currentNode = queue.poll();
+			ArrayList<Node> neighbours = currentNode.getAdjacentNodes();
+			for(Node n : neighbours){
+				if(distances[n.getLabel() - 1] == -1){
+					distances[n.getLabel() - 1] = distances[currentNode.getLabel() - 1] + EDGE_WEIGHT;
+					queue.add(n);
 				}
 			}
 		}
+		
+		return distances;
 	}
+	
 }
