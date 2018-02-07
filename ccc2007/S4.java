40b4e289dd63a7e2282c7496ffab3301ed7c5aea
paarthmadan <madanhockey@hotmail.com>
2018-02-07 10:57:25 -0500

    (add): S4 Complete, aggregated processed nodes, to eliminate repetition
    created a processed hashmap, containing the number of paths sprouting from a single node. if re-accessed, use the pre-calculated value as opposed to continuing the map traversal

--- ccc2007/S4.java
+++ ccc2007/S4.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class S4 {
 	public static int total = 0;
+	public static HashMap<Integer, Integer> processed = new HashMap<Integer, Integer>();
 	public static void main(String [] args){
 		Scanner input = new Scanner(System.in);
 		int n = input.nextInt();
@@ -16,24 +17,30 @@ public class S4 {
 			
 			if(a == 0 && b == 0)
 				break;
-			
-			if(map.get(b) == null){
+		
+			if(map.get(b) == null)
 				map.put(b, new ArrayList<Integer>());
-			}
+			
 			map.get(b).add(a);
 		}
 		traverseMap(n, map);
 		System.out.println(total);
 	}
 	public static void traverseMap(int startingKey, HashMap<Integer, ArrayList<Integer>> map){
+		int startingTotal = total;
 		if(map.containsKey(startingKey)){
 			for(int i : map.get(startingKey)){
 				if(i == 1){
 					total++;
-				}else{
+				}else if(processed.containsKey(i)){
+					total += processed.get(i);
+				}
+				else{
 					traverseMap(i, map);
 				}
 			}
 		}
+		int finalTotal = total;
+		processed.put(startingKey, finalTotal - startingTotal);
 	}
 }
