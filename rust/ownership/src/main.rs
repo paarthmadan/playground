c3ac39b965f4d5c52663ff815adbc2eac356b7ae
Paarth Madan <paarth.madan@shopify.com>
2018-12-02 20:43:17 -0500

    (rust): ownership, use slices to return first word

--- rust/ownership/src/main.rs
+++ rust/ownership/src/main.rs
@@ -5,6 +5,9 @@ fn main() {
 
     add_period(&mut s);
     println!("The newly formatted string is: {}", s);
+
+    let first_word = first_word(&s);
+    println!("The first word of the string is: {}", first_word);
 }
 
 fn calculate_length(s: &String) -> usize {
@@ -13,6 +16,18 @@ fn calculate_length(s: &String) -> usize {
 
 fn add_period(s: &mut String) {
     s.push_str("!");
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &letter) in bytes.iter().enumerate(){
+        if letter == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
 
 
