5355e84ce54b311ea4803e866c782aa19af946d3
Paarth Madan <paarth.madan@shopify.com>
2018-12-02 20:20:43 -0500

    (rust): ownership, changed signature to expect reference

--- rust/ownership/src/main.rs
+++ rust/ownership/src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
     let s = String::from("paarth's string");
-    let len = calculate_length(s);
+    let len = calculate_length(&s);
     println!("{}", len);
-    // Throws an error, because the ownership was moved into the function
     println!("{}", s);
 }
 
-fn calculate_length(s: String) -> usize {
+fn calculate_length(s: &String) -> usize {
     s.len()
 }
 
