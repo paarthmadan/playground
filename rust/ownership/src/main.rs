17de176e3a260ecb411cd448daf0bb0b3102832e
Paarth Madan <paarth.madan@shopify.com>
2018-12-02 20:26:51 -0500

    (rust): ownership, try to mutate borrowed reference (error)

--- rust/ownership/src/main.rs
+++ rust/ownership/src/main.rs
@@ -1,12 +1,18 @@
 fn main() {
     let s = String::from("paarth's string");
     let len = calculate_length(&s);
-    println!("{}", len);
-    println!("{}", s);
+    println!("The string '{}' has a length of {}", s, len);
+
+    add_period(&s);
+    println!("The newly formatted string is: {}", s);
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+
+fn add_period(s: &String) {
+    s.push_str("!");
 }
 
 
