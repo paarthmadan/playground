e33df91330abbb1a78e7290c2af5c39cad0be107
Paarth Madan <paarth.madan@shopify.com>
2018-12-02 20:18:19 -0500

    (rust): ownership, add code that exploits move/ownership change

--- rust/ownership/src/main.rs
+++ rust/ownership/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     let s = String::from("paarth's string");
     let len = calculate_length(s);
     println!("{}", len);
+    // Throws an error, because the ownership was moved into the function
+    println!("{}", s);
 }
 
 fn calculate_length(s: String) -> usize {
