1949d7b5c7813e0ffc476892716cecce65f55607
Paarth Madan <paarth.madan@shopify.com>
2019-02-23 14:23:31 -0500

    (2018): start s3 ruby, pseduo implementation

---
+++ ccc2018/s3.rb
@@ -0,0 +1,14 @@
+# Pseudo
+# 1. Create Node representing each cell
+# 2. Store possible routes from each cell, as edge, on each cell
+# 3. Store all nodes, with reference to their coordinate
+# 4. Use Dijkstra's to determine shortest path to each cell
+# 5. Output data, based on coordinate
+# 
+# Notes: 
+# All edges will have a cost of 1, so a conveyor belt can link two nodes
+# regardless of distance between them, and it'll be treated as 1 move 
+#
+# A preliminary screening will be run, to determine which nodes are affected
+# from the camera, and effectively remove these edges.
+
